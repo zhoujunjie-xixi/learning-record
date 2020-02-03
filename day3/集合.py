d67bc3751b9d3ce9e160316f7ed86abf1174364f
zhoujunjie-xixi <834217959@qq.com>
2020-02-03 10:20:17 +0800

    集合的操作

--- day3/集合.py
+++ day3/集合.py
@@ -1,7 +1,6 @@
-__author__ = "Alex Li"
-
+# 要创建一个set，需要提供一个list作为输入集合：
 list_1 = [1,4,5,7,3,6,7,9]
-list_1 = set(list_1)
+list_1 = set(list_1)    # set变集合
 
 list_2 =set([2,6,0,66,22,8,4])
 print(list_1,list_2)
